181737419b8d81066ed7d34b45bb27b2d400bdb5
Md. Ismail Hossain <hossain.eee042@gmail.com>
2023-12-25 18:38:05 +0600

    another unit test for decrement method

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -4,6 +4,7 @@ import 'package:unit_test_practice/counter.dart';
 
 void main() {
   //test("description what you are performing", () {action});
+  //Testing incremeant counter
   test("Testing the increment counter", () {
     //setup
     Counter counter = Counter(value: 5); // instance create
@@ -11,8 +12,18 @@ void main() {
     counter.increment(); // call increament method
     //test
     // expect(actual, matcher);
-    expect(counter.value, 6); 
-        // actual value is intial value and matcher is just next value of initial value which we set in setup
+    expect(counter.value, 6);
+    // actual value is intial value and matcher is just next value of initial value which we set in setup
     //now press testing icon from left side of vs code (chemistry lab icon ), after that debug test button on the top just right side of TESTING text.
+  });
+
+  //Testing decremeant counter
+  test("Testing the decrement counter", () {
+    //setup
+    Counter counter = Counter(value: 10);
+    //do
+    counter.decrement();
+    //test
+    expect(counter.value, 9);
   });
 }
