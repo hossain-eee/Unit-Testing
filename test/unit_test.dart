c0cc075790cc550427c3cfad2ac94f5b4365f637
Md. Ismail Hossain <hossain.eee042@gmail.com>
2023-12-25 18:43:31 +0600

    group test- several method test inside group

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -5,7 +5,7 @@ import 'package:unit_test_practice/counter.dart';
 void main() {
   //test("description what you are performing", () {action});
   //Testing incremeant counter
-  test("Testing the increment counter", () {
+/*   test("Testing the increment counter", () {
     //setup
     Counter counter = Counter(value: 5); // instance create
     //do
@@ -25,5 +25,28 @@ void main() {
     counter.decrement();
     //test
     expect(counter.value, 9);
+  }); */
+  //group test
+  group("Test the counter for both increment and decrement", () {
+      test("Testing the increment counter", () {
+    //setup
+    Counter counter = Counter(value: 5); // instance create
+    //do
+    counter.increment(); // call increament method
+    //test
+    // expect(actual, matcher);
+    expect(counter.value, 6);
+   
+  });
+
+  //Testing decremeant counter
+  test("Testing the decrement counter", () {
+    //setup
+    Counter counter = Counter(value: 10);
+    //do
+    counter.decrement();
+    //test
+    expect(counter.value, 9);
+  });
   });
 }
