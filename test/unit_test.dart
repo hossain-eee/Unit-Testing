44996506ba8cacc8e3e6cd30127ae4fe47ed4c2b
Md. Ismail Hossain <hossain.eee042@gmail.com>
2023-12-25 17:21:25 +0600

    testing has been performed

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:unit_test_practice/main.dart';
+import 'package:unit_test_practice/counter.dart';
 
 void main() {
   //test("description what you are performing", () {action});
   test("Testing the increment counter", () {
     //setup
+    Counter counter = Counter(value: 5); // instance create
     //do
+    counter.increment(); // call increament method
     //test
+    // expect(actual, matcher);
+    expect(counter.value, 6); 
+        // actual value is intial value and matcher is just next value of initial value which we set in setup
+    //now press testing icon from left side of vs code (chemistry lab icon ), after that debug test button on the top just right side of TESTING text.
   });
 }
